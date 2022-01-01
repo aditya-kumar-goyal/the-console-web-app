780c6cb9c14d8c7eb92872308abfb5403adc0e83
aditya-kumar-goyal <81676779+aditya-kumar-goyal@users.noreply.github.com>
2022-01-01 13:08:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("my first console-project")
+var first_name ="Aditya";
+console.log(first_name)
+var last_name ="kumar Goyal"
+console.log(last_name)
+var a =10;
+var b =5;
+var add = a + b ;
+console.log(add)
+var subtract = a - b ;
+console.log(subtract)
+var divide = a / b ;
+console.log(divide)
+var multiply = a * b ;
+console.log(multiply)
